6adac1e71a2bd2f0cb8a6e9ed71c9375b74d3415
Yolymar1208 <yolymarorfiano@yahoo.com>
2026-06-22 16:43:41 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,30 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          50:  '#E0F7F9',
-          100: '#B3ECF1',
-          200: '#80DFE8',
-          300: '#4DD2DF',
-          400: '#26C8D9',
-          500: '#4AB8C1',
-          600: '#0097A7',
-          700: '#00838F',
-          800: '#006064',
-          900: '#1E3A5F',
+          50:  '#E0FAFB',
+          100: '#B3F2F5',
+          200: '#7DE8ED',
+          300: '#3DD9E0',
+          400: '#00C9D4',
+          500: '#00B8C2',
+          600: '#0099A8',
+          700: '#007A88',
+          800: '#005C68',
+          900: '#0A0F1E',
         }
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      borderWidth: {
+        '0.5': '0.5px',
+      },
+      fontSize: {
+        '2xs': '10px',
+      },
+      letterSpacing: {
+        tight: '-0.3px',
+        tighter: '-0.5px',
       },
     },
   },
